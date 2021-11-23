219fc81c7732fe4b1364f6e931486b83629e6db6
Yuri Kriger <yuri.kriger@gmail.com>
2021-11-23 17:24:10 +0600

    DataInit

--- src/main/java/springboot/springBootMVC/dao/RoleRepository.java
+++ src/main/java/springboot/springBootMVC/dao/RoleRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import springboot.springBootMVC.model.Role;
+
+//Аннотация @EnableJpaRepositories активирует Spring Data JPA.
+// Spring Data JPA будет создавать конкретную реализацию для PersonRepository и настраивать на взаимодействие
+// с БД в памяти, используя JPA.
 @EnableJpaRepositories
 public interface RoleRepository extends JpaRepository<Role,Long> {
     Role findByName(String role);
